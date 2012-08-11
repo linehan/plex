75e6b3a8841a979c6c4c1c72c0f910fb3d12c469
Jason Linehan <patientulysses@gmail.com>
2012-08-11 19:54:29 -0400

    autogen script

---
+++ autogen.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Rename the HISTORY file to ChangeLog, so autotools doesn't have a cow
+mv HISTORY ChangeLog
+touch NEWS
+touch AUTHORS
+
+# Run the autotools routine
+   aclocal                \
+&& automake --add-missing \
+&& autoconf
+
+# Rename ChangeLog back to HISTORY
+mv ChangeLog HISTORY
